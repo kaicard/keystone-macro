3e5c5425737cb2dec5b96486cdef98461e3b732c
Kai C <kaicard05@gmail.com>
2026-03-27 21:54:34 +0000

    File changes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Navbar() {
             <Link to="/Home" className="flex items-center gap-3 group">
               <div className="w-8 h-8 rounded-full overflow-hidden shrink-0" style={{ background: 'transparent' }}>
                 <img
-                  src="https://media.base44.com/images/public/69b9efd1e34861737a4d8957/1b6645c56_BCD58650-3FC4-4E56-B335-136BA0F7A15D.png"
+                  src="https://media.base44.com/images/public/69b9efd1e34861737a4d8957/e2da08c1e_IMG_3747.jpg"
                   alt="The Macro Sphere"
                   className="w-full h-full"
                   style={{ transform: 'scale(3.6)', transformOrigin: '50% 34%', imageRendering: 'auto' }}
