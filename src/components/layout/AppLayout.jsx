a485ba463c5dcb82bfe70d568cc53f87ad0f4730
Kai C <kaicard05@gmail.com>
2026-03-27 21:23:48 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from '@/lib/ThemeContext';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import PageTransition from './PageTransition';
 
 export default function AppLayout() {
   return (
@@ -10,7 +11,9 @@ export default function AppLayout() {
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1">
-          <Outlet />
+          <PageTransition>
+            <Outlet />
+          </PageTransition>
         </main>
         <Footer />
       </div>
